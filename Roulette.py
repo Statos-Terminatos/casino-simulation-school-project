a6ddf9956432db8b3cfa32030855a2627ed36ef7
Aliya Ussinova <aussinova@gmail.com>
2017-01-15 19:54:50 +0100

    Tested pseudo random generation

---
+++ Roulette.py
@@ -0,0 +1,11 @@
+import random
+
+# This is used to fixed the random generator so we can test the output
+random.seed(3456)
+
+"""
+testing the pseudo random number generetor
+
+for _ in range(0, 5):
+    print(random.randrange(1, 10))
+"""
