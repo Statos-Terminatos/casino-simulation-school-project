240aa9a16b642c51746608193629d5fbca6dfc81
Aliya Ussinova <aussinova@gmail.com>
2017-01-27 11:01:53 +0100

    In craps added function to calculated the amount players win, removed trial files Roulette2 and craps2. Finished the merging of the games.

--- Simulation.py
+++ Simulation.py
@@ -6,28 +6,28 @@ from Employee import Barman, Croupier
 # This is used to fixe the pseudo random generator so we can test the output
 
 
-# res = []
-# total1 = 0
-# total2 = 0
-# for i in range(10000):
-#     craps = Craps.Craps(0.9, 1)
-#     table = Roulette.Roulette(1)
-#     amounts1 = [random.randrange(50, 200) for x in range(10)]
-#     amounts2 = [random.randrange(40, 300) for x in range(10)]
-#     res1 = craps.SimulateGame(amounts1)
-#     res2 = table.SimulateGame(amounts2)
-#     print("--------------------------\n")
-#     total1 = total1 + sum(res1[1])/sum(amounts1)
-#     total2 = total2 + sum(res2[1])/sum(amounts2)
-#     print( "total: {}%".format(total1) )
-#     print( "total: {}%".format(total2) )
-#     print("--------------------------\n")
+res = []
+total1 = 0
+total2 = 0
+for i in range(10000):
+    craps = Craps.Craps(0.9, 1)
+    table = Roulette.Roulette(1)
+    amounts1 = [random.randrange(50, 200) for x in range(10)]
+    amounts2 = [random.randrange(40, 300) for x in range(10)]
+    res1 = craps.SimulateGame(amounts1)
+    res2 = table.SimulateGame(amounts2)
+    print("--------------------------\n")
+    total1 = total1 + sum(res1[1])/sum(amounts1)
+    total2 = total2 + sum(res2[1])/sum(amounts2)
+    print( "total: {}%".format(total1) )
+    print( "total: {}%".format(total2) )
+    print("--------------------------\n")
 
-# print(total1/10000*100)
-# print(total2/10000*100)
+print(total1/10000*100)
+print(total2/10000*100)
 
-b = Barman("aliya", 300)
-print(b)
+#b = Barman("aliya", 300)
+#print(b)
 
 
 
